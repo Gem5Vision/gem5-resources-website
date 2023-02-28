43c9823f0622f2f7862900d471bb261959fd4ea3
helloparthshah <helloparthshah@users.noreply.github.com>
2023-02-28 02:57:56 +0000

    deploy: 783d45f57cc941122d1c14cb91fd1fd36c26518c

--- _next/static/chunks/pages/resources/[id]/[[...page]]-d3cb074b3d75c126.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[481],{9163:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/resources/[id]/[[...page]]",function(){return u(4455)}])}},function(n){n.O(0,[475,337,455,774,888,179],function(){return n(n.s=9163)}),_N_E=n.O()}]);
