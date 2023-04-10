4cb20e9e84dcf48861de21faa5ee796b4e9d56b3
Harshil2107 <Harshil2107@users.noreply.github.com>
2023-04-10 23:35:48 +0000

    deploy: 7620cb093eba93cbaec9b311ce720e8b22db5771

---
+++ _next/static/chunks/pages/resources/[id]/[[...page]]-8cacdebeda017d60.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[481],{9163:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/resources/[id]/[[...page]]",function(){return u(3250)}])}},function(n){n.O(0,[936,901,869,250,774,888,179],function(){return n(n.s=9163)}),_N_E=n.O()}]);
